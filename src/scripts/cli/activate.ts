5a86f75b61c788321d61460ab8976953c2d1985f
Romuald Brillout <github.com@brillout.com>
2020-09-13 13:13:05 +0200

    wip

--- src/scripts/cli/activate.ts
+++ src/scripts/cli/activate.ts
@@ -1,7 +1,3 @@
-/*
-import { UserConfig } from "../UserConfig";
-import { splitByLine } from "../utils/split";
-*/
 import { symbolInfo, symbolSuccess, stylePath } from "@lsos/utils";
 import { writeFileSync, readFileSync } from "fs";
 import { join as pathJoin } from "path";
@@ -42,52 +38,7 @@ async function activate(keyEncoded: string): Promise<void> {
       stylePath(await getLsosConfigPath()) +
       "."
   );
-
-  /*
-  const isAlreadyRemoved: boolean = UserConfig.get()?.donationReminder?.remove;
-
-  if (!isAlreadyRemoved) {
-    UserConfig.set({
-      donationReminder: {
-        remove: true,
-      },
-    });
-  }
-
-  console.log(
-    (isAlreadyRemoved ? symbolInfo : symbolSuccess) +
-      "Lsos config " +
-      (isAlreadyRemoved ? "" : "saved ") +
-      "at " +
-      stylePath(UserConfig.configFilePath) +
-      ":"
-  );
-  console.log(prettyUserConfig());
-
-  console.log();
-  if (isAlreadyRemoved) {
-    console.log(symbolInfo + "Donation-reminder already removed.");
-  } else {
-    console.log(symbolSuccess + "Donation-reminder successfully removed.");
-  }
-  console.log();
-
-  console.log(
-    symbolInfo + "More info at https://github.com/Lsos/donation-reminder."
-  );
-  console.log();
-  */
 }
-
-/*
-function prettyUserConfig() {
-  const userConfig = UserConfig.get();
-  const lines = splitByLine(JSON.stringify(userConfig, null, 2));
-  const TAB = "    ";
-  const withTabs = lines.map((line) => TAB + line).join(EOL);
-  return withTabs;
-}
-*/
 
 async function addActivationKey(
   activationKey: ActivationKey
